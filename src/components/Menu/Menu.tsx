5bd3bc847a29ac602cc3c31b2d5d44f3478dc1d9
JosueRauda7 <josuerauda.dev@gmail.com>
2025-05-18 17:37:50 -0600

    Menu finalizado, estilo

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,30 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "./Menu.css";
 
 const Menu = () => {
+  const [isMobile, setIsMobile] = useState(false);
   const [showMenu, setShowMenu] = useState(true);
 
+  useEffect(() => {
+    if(window.innerWidth < 768) {
+      setIsMobile(true);
+      setShowMenu(false);
+    }
+  }, []);
+
   return (
-    <div className="menu flex md:flex-col lg:flex-row items-center bg-cyan-950 text-white p-4 shadow-lg">
-      <div className="logo flex items-center">
-        <span className="text-xl font-bold">TallerGo</span>
-      </div>
-      <nav className="ml-auto cursor-pointer">
-        <div onClick={() => setShowMenu(!showMenu)} className="lg:hidden md:flex flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
+    <div>
+      <div className="menu flex md:flex-col lg:flex-row items-center bg-cyan-950 text-white p-4 shadow-lg">
+        <div className="logo flex items-center">
+          <span className="text-xl font-bold">TallerGo</span>
         </div>
-        <ul className="lg:flex lg:flex-row md:flex-row sm:flex-col space-x-4">
-          {showMenu && (
-            <>
-              <li><a href="#home" className="text-white-700 hover:text-white-900">Inicio</a></li>
-              <li><a href="#talleres" className="text-white-700 hover:text-white-900">Talleres</a></li>
-              <li><a href="#about" className="text-white-700 hover:text-white-900">Acerca de</a></li>
-              <li><a href="#contact" className="text-white-700 hover:text-white-900">Contacto</a></li>
-            </>
+        <nav className="ml-auto cursor-pointer">
+          <div onClick={() => setShowMenu(!showMenu)} className="lg:hidden md:flex flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </div>
+          {!isMobile && showMenu && (
+            <ul className="lg:flex lg:flex-row md:flex-row sm:flex-col space-x-4">
+                <>
+                  <li className="nav-option-desktop text-gray-100 hover:text-shadow-2xs hover:text-white cursor-pointer">Inicio</li>
+                  <li className="nav-option-desktop text-gray-50 hover:text-white cursor-pointer">Talleres</li>
+                  <li className="nav-option-desktop text-gray-50 hover:text-white cursor-pointer">Acerca de</li>
+                  <li className="nav-option-desktop text-gray-50 hover:text-white cursor-pointer">Contáctanos</li>
+                </>
+            </ul>
           )}
+        </nav>
+      </div>
+      {isMobile && showMenu && (
+        <ul className="lg:flex lg:flex-row md:flex-row sm:flex-col space-x-4 bg-cyan-950 shadow-lg">
+            <>
+              <li className="text-gray-100 p-4 hover:text-white hover:bg-cyan-700 active:text-white active:bg-cyan-800 cursor-pointer">Inicio</li>
+              <li className="text-gray-100 p-4 hover:text-white hover:bg-cyan-700 active:text-white active:bg-cyan-800 cursor-pointer">Talleres</li>
+              <li className="text-gray-100 p-4 hover:text-white hover:bg-cyan-700 active:text-white active:bg-cyan-800 cursor-pointer">Acerca de</li>
+              <li className="text-gray-100 p-4 hover:text-white hover:bg-cyan-700 active:text-white active:bg-cyan-800 cursor-pointer">Contáctanos</li>
+            </>
         </ul>
-      </nav>
+      )}
     </div>
   );
 }
